ffe388cc7b3f26a8e648c4e5fedf4f55dffa33ad
BarbosaRT <barbosart137@gmail.com>
2026-01-31 17:42:42 -0300

    feat: implement 404 page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold">404</h1>
+      <p className="mt-4">
+        Página não encontrada
+      </p>
+    </div>
+  );
+}
