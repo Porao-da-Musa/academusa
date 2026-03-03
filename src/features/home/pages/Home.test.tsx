07536205824fc2868d3c4141ae5ca5e57768a91f
KauanQuinzote <kauandominguesdesouza@gmail.com>
2026-03-03 19:59:18 -0300

    fix done

--- src/features/home/pages/Home.test.tsx
+++ src/features/home/pages/Home.test.tsx
@@ -1,17 +1,21 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter, MemoryRouter, Route, Routes } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 import { Home } from "./Home";
 import { describe, it, expect } from "vitest";
+import { MyWorkout } from "@features/my-workout/pages/MyWorkout";
 
 describe("Home", () => {
-  it("should render Treino de Hoje component", () => {
+  it("should render Home component", () => {
     render(
       <BrowserRouter>
         <Home />
       </BrowserRouter>,
     );
 
-    const treinoDeHojeHeading = screen.getByText("Treino de Hoje");
+    const treinoDeHojeHeading = screen.getByRole("heading", {
+      name: /treino de hoje/i,
+    });
     expect(treinoDeHojeHeading).toBeInTheDocument();
   });
 
@@ -26,16 +30,31 @@ describe("Home", () => {
     expect(treinoDeHojeHeading).toBeInTheDocument();
   });
 
-  it("Should redirect to /treinos when 'Iniciar Treino' is clicked", () => {
+  it("Should redirect to /exercises when 'Iniciar Treino' is clicked", async () => {
     render(
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>,
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home/my-workout" element={<MyWorkout />} />
+        </Routes>
+      </MemoryRouter>,
     );
 
-    const iniciarTreinoButton = screen.getByText("Iniciar Treino");
+    const iniciarTreinoButton = screen.getByRole("button", {
+      name: /iniciar treino/i,
+    });
     expect(iniciarTreinoButton).toBeInTheDocument();
 
-    iniciarTreinoButton.click();
+    await userEvent.click(iniciarTreinoButton);
+
+    await waitFor(
+      () => {
+        const treinoDeHojeHeading = screen.getByRole("heading", {
+          name: /Bem-vindo à Página Meu Treino!/i,
+        });
+        expect(treinoDeHojeHeading).toBeInTheDocument();
+      },
+      { timeout: 2000 },
+    );
   });
 });
