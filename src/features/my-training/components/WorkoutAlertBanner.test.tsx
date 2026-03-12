2c0dff5f5a3196d89270b8260aad916c84bb0395
kaiqueteixeiradev <kaiqueteixeiradev@gmail.com>
2026-03-12 14:07:27 -0300

    fix alertbanner test

--- src/features/my-training/components/WorkoutAlertBanner.test.tsx
+++ src/features/my-training/components/WorkoutAlertBanner.test.tsx
@@ -5,13 +5,13 @@ import { WorkoutAlertBanner } from "./WorkoutAlertBanner";
 test("should render alert when hasOccupied is true", () => {
   render(<WorkoutAlertBanner hasOccupied={true} />);
 
-  const alert = screen.getByText(/alguns equipamentos estão ocupados/i);
+  const alert = screen.queryByText(/alguns equipamentos estão ocupados/i);
   expect(alert).toBeInTheDocument();
 });
 
 test("should not render alert when hasOccupied is false", () => {
   render(<WorkoutAlertBanner hasOccupied={false} />);
 
-  const alert = screen.getByText(/alguns equipamentos estão ocupados/i);
+  const alert = screen.queryByText(/alguns equipamentos estão ocupados/i);
   expect(alert).not.toBeInTheDocument();
 });
