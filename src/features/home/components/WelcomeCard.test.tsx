e180ed2084ed6e346b257ee4809cdef4542c146c
kaiqueteixeiradev <kaiqueteixeiradev@gmail.com>
2026-03-17 18:04:58 -0300

    fix test welcomecard

--- src/features/home/components/WelcomeCard.test.tsx
+++ src/features/home/components/WelcomeCard.test.tsx
@@ -1,6 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import { WelcomeCard } from "./WelcomeCard";
+import { vi } from "vitest";
+
+vi.mock("../../../services/supabaseClient", () => ({
+  supabase: {
+    auth: {
+      getUser: vi.fn().mockResolvedValue({
+        data: {
+          user: {
+            user_metadata: { name: "Kaique" },
+          },
+        },
+        error: null,
+      }),
+    },
+  },
+}));
 
 describe("Componente WelcomeCard", () => {
   it("it should display the full greeting when a dynamic name is passed via props", () => {
