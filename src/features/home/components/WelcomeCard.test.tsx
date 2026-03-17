960874bfb1ea50f3be68f44d4ac411daa204f56e
kaiqueteixeiradev <kaiqueteixeiradev@gmail.com>
2026-03-17 18:11:11 -0300

    fix test

--- src/features/home/components/WelcomeCard.test.tsx
+++ src/features/home/components/WelcomeCard.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { WelcomeCard } from "./WelcomeCard";
-import { vi } from "vitest";
 
 vi.mock("../../../services/supabaseClient", () => ({
   supabase: {
@@ -9,7 +8,7 @@ vi.mock("../../../services/supabaseClient", () => ({
       getUser: vi.fn().mockResolvedValue({
         data: {
           user: {
-            user_metadata: { name: "Kaique" },
+            user_metadata: { name: "UsuarioTeste123" },
           },
         },
         error: null,
@@ -19,13 +18,13 @@ vi.mock("../../../services/supabaseClient", () => ({
 }));
 
 describe("Componente WelcomeCard", () => {
-  it("it should display the full greeting when a dynamic name is passed via props", () => {
-    const mockUserName = "UsuarioTeste123";
+  it("should display the full greeting with user name", async () => {
     render(<WelcomeCard />);
 
     expect(
-      screen.getByText(`Bem vindo de volta, ${mockUserName}!`),
+      await screen.findByText("Bem-vindo(a) de volta, UsuarioTeste123!"),
     ).toBeInTheDocument();
+
     expect(
       screen.getByText("Pronto para começar seu treino?"),
     ).toBeInTheDocument();
