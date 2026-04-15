781c2c5745bcc85c55892d92ae24df1f026a47cb
kaiqueteixeiradev <kaiqueteixeiradev@gmail.com>
2026-04-14 21:02:46 -0300

    fix mobile button

--- src/features/my-training/components/AddExerciseButton.tsx
+++ src/features/my-training/components/AddExerciseButton.tsx
@@ -8,9 +8,10 @@ export function AddExerciseButton({ onOpenAddExercise }: Props) {
   return (
     <button
       onClick={onOpenAddExercise}
-      className="flex items-center gap-2 rounded-lg bg-blue-600 px-5 py-3 text-sm font-normal text-white transition hover:bg-blue-700"
+      className="
+    flex items-center gap-1.5 rounded-lg bg-blue-600  px-3 py-2 text-xs sm:px-5 sm:py-3 sm:text-sm font-normal text-white transition hover:bg-blue-700"
     >
-      <span className="text-lg leading-none">+</span>
+      <span className="text-base sm:text-lg leading-none">+</span>
       Adicionar Exercício
     </button>
   );
